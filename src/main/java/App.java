fd8b54ffc8b73e993ae1a0598573108a7fa99679
the holla <holla@bikefarm.org>
2015-08-30 23:08:26 -0700

    Routes functional

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,19 +4,20 @@ import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
-
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    /* Index */
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/tasks.vtl");
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    get("/tasks", (request,response) -> {
+    /* Index --> Tasks*/
+    get("/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       List<Task> tasks = Task.all();
       model.put("tasks", tasks);
@@ -24,38 +25,78 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    /* Index --> Categories*/
+    get("/categories", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Category> categories = Category.all();
+      model.put("categories", categories);
+      model.put("template", "templates/categories.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    /* Task list/form --> See a particular task */
     get("/tasks/:id", (request,response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       int id = Integer.parseInt(request.params("id"));
       Task task = Task.find(id);
       model.put("task", task);
+      model.put("allCategories", Category.all());
       model.put("template", "templates/task.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    /* Category list/form --> See a particular category */
+    get("/categories/:id", (request,response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Category category = Category.find(id);
+      model.put("category", category);
+      model.put("allTasks", Task.all());
+      model.put("template", "templates/category.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    /* Task list/form --> POST a new task */
     post("/tasks", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       String description = request.queryParams("description");
       Task newTask = new Task(description);
+      newTask.save();
       response.redirect("/tasks");
       return null;
     });
 
-    put("/tasks/:id", (request, response) -> {
+    /* Category list/form --> POST a new category */
+    post("/categories", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      Task task = Task.find(Integer.parseInt(request.params("id")));
-      String description = request.queryParams("description");
-      task.update("description");
-      model.put("template", "templates/task.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+      String name = request.queryParams("name");
+      Category newCategory = new Category(name);
+      newCategory.save();
+      response.redirect("/categories");
+      return null;
+    });
 
-    delete("/tasks/:id", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      Task task = Task.find(Integer.parseInt(request.params("id")));
-      task.delete();
-      model.put("template", "templates/task.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+    /* Category page --> POST a task to this category */
+    post("/add_tasks", (request, response) -> {
+      int taskId = Integer.parseInt(request.queryParams("task_id"));
+      int categoryId = Integer.parseInt(request.queryParams("category_id"));
+      Category category = Category.find(categoryId);
+      Task task = Task.find(taskId);
+      category.addTask(task);
+      response.redirect("/categories/" + categoryId);
+      return null;
+    });
+
+    /* Task page --> POST a category to this task */
+    post("/add_categories", (request, response) -> {
+      int taskId = Integer.parseInt(request.queryParams("task_id"));
+      int categoryId = Integer.parseInt(request.queryParams("category_id"));
+      Category category = Category.find(categoryId);
+      Task task = Task.find(taskId);
+      task.addCategory(category);
+      response.redirect("/tasks/" + taskId);
+      return null;
+    });
+
   }
 }
